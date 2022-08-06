55dee763907f252d36b3429c1679b6cd4bf7de33
Erik Braga <109657283+ErikBraga@users.noreply.github.com>
2022-08-06 03:28:28 -0300

    exercícios-curso-python

---
+++ Exercícios basicos/Condições (If-Else)/Par ou Ímpar.py
@@ -0,0 +1,5 @@
+num = int(input('Me diga um número qualquer: '))
+if num % 2 == 0:  # calcula se a variável "num" é divisivel por 2
+    print(f'O número {num} é PAR')
+else:
+    print(f'O número {num} é ÍMPAR')
