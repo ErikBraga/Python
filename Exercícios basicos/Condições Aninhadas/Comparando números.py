10ed24ddf7c9a5f6dfa0c9cf141189d019788d31
Erik Braga <109657283+ErikBraga@users.noreply.github.com>
2022-08-08 04:15:18 -0300

    exercícios-curso-python

---
+++ Exercícios basicos/Condições Aninhadas/Comparando números.py
@@ -0,0 +1,8 @@
+p1 = int(input('Primeiro número: '))
+p2 = int(input('Segundo número: '))
+if p1 > p2:
+    print('O PRIMEIRO valor é maior.')
+elif p2 > p1:
+    print('O SEGUNDO valor é maior.')
+else:
+    print('Os dois valores são iguais.')
